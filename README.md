9937f225123d4312bb601bd0a993d5295ad1a4d5
chenjinya <jinyachen@gmail.com>
2016-08-19 23:15:51 +0800

    update

--- README.md
+++ README.md
@@ -3,24 +3,20 @@ a react demo
 
 
 start:
-<<<<<<< HEAD
 
-webpack develop
-#npm run dev
+##webpack develop
+npm run dev
 
-webpack build
-#npm run build
+##webpack build
+npm run build
 
-gulp build
-#rm -rf build && gulp build
+##gulp build
+rm -rf build && gulp build
 
-部署到服务器
-#cd output
-#sh dev.sh
+##部署到服务器
+cd output
+sh dev.sh
 
 
-#生成tar包在/home/wor/orp下，测试的时候这样 部署
-#tar xzvf ala.tar.gz -C template/ && tar xzvf static-ala.tar.gz -C /home/work/static/static.tieba.baidu.com/
-=======
-$node node_modules/webpack-dev-server/bin/webpack-dev-server.js --content-base build/
->>>>>>> c23892bd314d1b607cc9c5ef5ebca893e7f2238b
+生成tar包在/home/wor/orp下，测试的时候这样 部署
+tar xzvf ala.tar.gz -C template/ && tar xzvf static-ala.tar.gz -C /home/work/static/static.tieba.baidu.com/
--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,6 @@ var webpack = require('webpack');
 var path = require('path');
 var autoprefixer = require('autoprefixer');
 
-<<<<<<< HEAD
-module.exports = {
-  entry: {
-    main: [
-      //dev hot reload
-      'webpack-dev-server/client?http://0.0.0.0:8080',//资源服务器地址
-      'webpack/hot/only-dev-server',
-      //dev hot reload
-      path.resolve(__dirname, './app/richAnchor/main.jsx'),
-    ]
-  },
-=======
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: [
@@ -25,7 +13,6 @@ module.exports = {
     //dev hot reload
     path.resolve(__dirname, './app/main.jsx'),
   ],
->>>>>>> c23892bd314d1b607cc9c5ef5ebca893e7f2238b
 
   output: {
     publicPath: "http://127.0.0.1:8080/dist/",
@@ -41,7 +28,6 @@ module.exports = {
       },
       {
         test: /\.jsx$/,
-<<<<<<< HEAD
         loaders: ['react-hot', 'babel']
       },
       {
@@ -51,17 +37,6 @@ module.exports = {
       {
         test: /\.css$/,
         loaders: ['style', 'css']
-=======
-        loader: 'react-hot!babel-loader!jsx-loader?harmony'
-      },
-      {
-        test: /\.less$/,
-        loader: 'style-loader!css-loader!postcss-loader!less-loader'
-      }, // use ! to chain loaders
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
->>>>>>> c23892bd314d1b607cc9c5ef5ebca893e7f2238b
       },
       {
         test: /\.(png|jpg)$/,
@@ -69,7 +44,6 @@ module.exports = {
       } // inline base64 URLs for <=8k images, direct URLs for the rest
     ]
   },
-<<<<<<< HEAD
   postcss: [ autoprefixer({ browsers: ['> 5%'] }) ],
   devServer: {
     hot: true,
@@ -79,32 +53,19 @@ module.exports = {
     proxy: {
       '/ala/*': {
           target: 'http://haoyunfeng.tieba.otp.baidu.com/',
-=======
-  postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ],
-  devServer: {
-    hot: true,
-    publicPath: "http://127.0.0.1:8080/dist/",
-    contentBase: "app/",
-    proxy: {
-      '/ala/*': {
-          target: 'http://haoyunfeng.service.tieba.otp.baidu.com:8080',
->>>>>>> c23892bd314d1b607cc9c5ef5ebca893e7f2238b
           secure: false
       }
     }
   },
 
   plugins: [
-<<<<<<< HEAD
     new webpack.optimize.CommonsChunkPlugin('vendor',  'vendor.js'),
-=======
     // commonsPlugin,
     // new HtmlWebpackPlugin({  // Also generate a test.html
     //   filename: 'index.html',
     //   title: 'ALa 主播大集合',
     //   template: path.resolve(__dirname, './app/index.html')
     // }),
->>>>>>> c23892bd314d1b607cc9c5ef5ebca893e7f2238b
     new webpack.DefinePlugin({
         'process.env.NODE_ENV': '"development"'
     }),
